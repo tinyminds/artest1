dad151507eb8803f25f252e59ed159ab91fe498f
jennyrolfe <jennyr@epic.co.uk>
2019-01-21 15:01:10 +0000

    turn to face and positioning

---
+++ arfoundation!1/Assets/Scripts/TurnToFace.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnToFace : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 transPos = transform.position;
+		transform.LookAt(transPos + Camera.main.transform.rotation * Vector3.forward,
+			Camera.main.transform.rotation * Vector3.up);
+    }
+}
